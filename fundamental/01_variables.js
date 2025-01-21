6e066a31476c008efe9e5e561c989edcf0c84ee0
Success-1 <itumasuccess29@gmail.com>
2025-01-21 22:38:58 +0100

    first commit

---
+++ fundamental/01_variables.js
@@ -0,0 +1,45 @@
+// Variables and Values
+let js = "Amazing"
+console.log(js);
+
+console.log(40 + 20 + 30 + 10);
+
+console.log("Success");
+console.log(23);
+
+let firstname = "success"
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+// Javascript reserved keywords can't be used for variable names
+// Eg: function, let, const, var, log, name etc
+// Dont use an uppercase to strt a javascript variable name. It is not conventional, it's used for a specific case in javascript in OOP
+
+// Proper Variable Naming. Eg:
+let myFirstJob = "Programmer"
+let myCurrentJob = "Teacher"
+
+// Assignment 1 On Values and Variables
+
+// 1. Declare variables called country, continents, and population and assign their values according to your own country(population in millions)
+// 2. Log their values to the console
+
+// Answer
+
+// No.1
+
+let country = "Nigeria"
+let continent = "Africa"
+let population = 232679478
+
+console.log(country);
+console.log(continent);
+console.log(popu);
+
+
+
+
+
+
---
+++ fundamental/02_data-types.js
@@ -0,0 +1,22 @@
+// Data types
+
+//Primitive and Objects
+// PRIMITIVE:
+// 1. Numbers - Floating points even when the points are not visible - decimals or integers 
+// Eg: 
+let age = 20
+let centimeter = 0.03
+//2. String - Sequence of characters - used for text 
+// Eg: 
+let firstName = "Success"
+
+// 3. Bolean - Logical type that can only be true  or false - used for taking deciscion
+
+let FullAge = true
+
+// 3. Undefined - Value taken by the variables without a value or it's value is not defined (empty vallue). Eg: let children
+
+// 4. Null - Also means 'empty value'
+
+// 5. Symbol(ES2015) - Value that is unique and cannot be changed(NOT USEFUL FOR NOW)
+// BigInt(ES2020) - Large integers than the number typpe can hold
